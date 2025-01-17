71f84cc3d9b0927c6ee5254b59790649201de431
Vianney Marticou <vianney.marticou@epita.fr>
2025-01-17 21:10:56 +0100

    fix: websiteUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://mrvym.github.io/',
+  url: 'https://mrvym.github.io/blog',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
