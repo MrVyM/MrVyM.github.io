3ea2c355d8fa99ae9c53cfdcd90597684062fa70
MrVyM <MrVyM@users.noreply.github.com>
2025-01-20 13:36:01 +0000

    deploy: d1d2630d7206954181e5c5b5eada3f81875f1953

--- assets/js/acecf23e.888e9bf1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_blog=self.webpackChunkmy_blog||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/posts/blog","blogTitle":"Blog","authorsListPath":"/posts/blog/authors"}')}}]);
